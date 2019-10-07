648007266736e94109cb09820bd4969aec02f7fb
Alex <agordon1123@gmail.com>
2019-10-07 12:41:51 -0400

    added friend image

--- client/employees/src/components/Friend.js
+++ client/employees/src/components/Friend.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Card } from 'semantic-ui-react';
+import { Card, Image } from 'semantic-ui-react';
 
 const Friend = ({ props }) => {
     return (
-        <div>
+        <div className='friend'>
             <Card.Group>
                 <Card>
+                    <Image src='https://www.mountaineers.org/images/placeholder-images/placeholder-contact-profile/image_preview' />
                     <Card.Content>
                         <Card.Header content={props.username} />
                         <Card.Meta content={props.department} />
