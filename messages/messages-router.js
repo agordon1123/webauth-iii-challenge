edf6362e55b749f53389f1dd1e614c322af71b34
Alex <agordon1123@gmail.com>
2019-10-09 09:44:59 -0400

    conditionalized messages router return

--- messages/messages-router.js
+++ messages/messages-router.js
@@ -9,7 +9,11 @@ router.get('/:id', (req, res) => {
     Messages.findByUserId(id)
         .then(succ => {
             console.log(succ);
-            res.status(200).json(succ);
+            if (succ === undefined) {
+                res.status(404).json({ message: "No messages found for this user" });
+            } else {
+                res.status(200).json(succ);
+            }
         })
         .catch(err => res.status(500).json(err));
 });
